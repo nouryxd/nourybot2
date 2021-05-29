c69880aacfcb423c8bffd436a425ca8d5e9b3343
lyx0 <stefan.parfuss@protonmail.ch>
2021-05-29 11:09:18 +0200

    add prefix in botinfo command

--- commands/botinfo.go
+++ commands/botinfo.go
@@ -9,6 +9,6 @@ import (
 
 func HandleBotinfo(channel string) {
 	botUptime := fmt.Sprint(humanize.Time(bot.Nourybot.Uptime))
-	reply := fmt.Sprintf("Twitch bot written in Go by @nouryqt. Last restart: %s. Commands: https://gist.github.com/lyx0/161913eb719afacea578b47239d0d969", botUptime)
+	reply := fmt.Sprintf(`Twitch bot written in Go by @nouryqt. Prefix: "()". Last restart: %s. Commands: https://gist.github.com/lyx0/161913eb719afacea578b47239d0d969`, botUptime)
 	bot.SendTwitchMessage(channel, reply)
 }
