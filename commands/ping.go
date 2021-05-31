b636baf4f2ba144d49b8500c858c7524ecef564f
lyx0 <stefan.parfuss@protonmail.ch>
2021-05-31 19:31:05 +0200

    refactor, add humanize

--- commands/ping.go
+++ commands/ping.go
@@ -1,14 +1,11 @@
 package commands
 
 import (
-	"fmt"
-
-	"github.com/dustin/go-humanize"
 	bot "github.com/lyx0/nourybot-go/bot"
+	hum "github.com/lyx0/nourybot-go/humanize"
 )
 
 func HandlePing(channel string) {
-	botUptime := fmt.Sprint(humanize.Time(bot.Nourybot.Uptime))
+	botUptime := hum.HumanizeTime(bot.Nourybot.Uptime)
 	bot.SendTwitchMessage(channel, "Pong! :) Last restart: "+botUptime+".")
-
 }
