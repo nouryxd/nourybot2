5bdf64368a8db8eb7fd65985bbdf41719295d822
lyx0 <stefan.parfuss@protonmail.ch>
2021-06-02 19:08:28 +0200

    clean up, add comments

--- humanize/main.go
+++ humanize/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/dustin/go-humanize"
 )
 
+// HumanizeTime returns a more human readable
+// time as a string for a given time.Time
 func HumanizeTime(t time.Time) string {
 	return humanize.Time(t)
 }
--- modules/main.go
+++ modules/main.go
@@ -11,6 +11,8 @@ import (
 	common "github.com/lyx0/nourybot-go/common"
 )
 
+// ConnectDatabase connects to my MySQL database
+// and calls JoinChannels and AnnounceJoin
 func ConnectDatabase() (db *sql.DB) {
 	envErr := godotenv.Load()
 	if envErr != nil {
@@ -22,7 +24,12 @@ func ConnectDatabase() (db *sql.DB) {
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_NAME := os.Getenv("DB_NAME")
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", DB_USER, DB_PASS, DB_HOST, DB_NAME))
+	db, err := sql.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:3306)/%s",
+			DB_USER,
+			DB_PASS,
+			DB_HOST,
+			DB_NAME))
 	defer db.Close()
 
 	if err != nil {
@@ -37,6 +44,7 @@ func ConnectDatabase() (db *sql.DB) {
 
 	// Get each channel from database and join them
 	common.JoinChannels(db)
+	// Get a set of channels where we should announce when we join
 	common.AnnounceJoin(db)
 
 	return db
