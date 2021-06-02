934c612fcab9edb8b63feefabfaed16122eb8c3d
lyx0 <stefan.parfuss@protonmail.ch>
2021-06-02 19:27:36 +0200

    add comments to connectchannels.sql

--- db/connectchannels.sql
+++ db/connectchannels.sql
@@ -6,7 +6,12 @@ CREATE TABLE `connectchannels` (
   `Announce` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
+-- Name is Twitch username.
+-- Platform is Twitch.
+-- Platform_ID is the userid of Name.
+-- Connect is if we should join.
+-- Announce is if we should say something when we join.
 INSERT INTO `connectchannels` (`Name`, `Platform`, `Platform_ID`, `Connect`, `Announce`) VALUES
 ('nouryqt', 'Twitch', '31437432 ', 'true', 'true'),
 ('nourybot', 'Twitch', '596581605 ', 'true', 'false');
+COMMIT;
