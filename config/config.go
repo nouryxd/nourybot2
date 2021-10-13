a11273e7c6aafab838c759f14440d0cbf02f8e2b
lyx0 <stefan@parfuss.me>
2021-10-13 20:57:14 +0200

    remove db

--- config/config.go
+++ config/config.go
@@ -10,10 +10,6 @@ import (
 type Config struct {
 	Username string
 	Oauth    string
-	DB_Name  string
-	DB_User  string
-	DB_Pass  string
-	DB_Host  string
 }
 
 func LoadConfig() *Config {
@@ -25,10 +21,6 @@ func LoadConfig() *Config {
 	cfg := &Config{
 		Username: os.Getenv("TWITCH_USER"),
 		Oauth:    os.Getenv("TWITCH_PASS"),
-		DB_Name:  os.Getenv("DB_NAME"),
-		DB_User:  os.Getenv("DB_USER"),
-		DB_Pass:  os.Getenv("DB_PASS"),
-		DB_Host:  os.Getenv("DB_HOST"),
 	}
 	log.Info("Config loaded succesfully")
 
